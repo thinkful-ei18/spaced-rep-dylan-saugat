7544af1d4d5ca68b1574094d5f93918ba8ee48fd
Dylan <nielsen.dylan@gmail.com>
2018-04-17 13:28:34 -0700

    Add error first error handling and delete superfluous console logs

--- db-mongoose.js
+++ db-mongoose.js
@@ -6,7 +6,6 @@ mongoose.Promise = global.Promise;
 const { DATABASE_URL } = require('./config');
 
 function dbConnect(url = DATABASE_URL) {
-  console.log(process.env.DATABASE_URL);
   return mongoose.connect(url)
     .catch(err => {
       console.error('Mongoose failed to connect');
